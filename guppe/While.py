35eb428b677ca8afd56683e23263fff8dbee4fc1
Aleeeeeh <alefesilva097@gmail.com>
2020-07-07 20:47:08 -0300

    Python

---
+++ guppe/While.py
@@ -0,0 +1,14 @@
+"""
+Loop While
+
+Bloco While será repetido enquanto a expressão for verdadeira
+
+Saindo de loops com Break
+
+
+
+"""
+resposta = ''
+
+while resposta != 'sim':
+    resposta = input('Acabou Jéssica: ')
